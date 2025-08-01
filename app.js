8b5b1a0251b627a8da6ca53fe876cb0075a9140e
Zefhiros <alejandropizarroleon@gmail.com>
2025-08-01 10:34:02 -0400

    Se actualiza app.js y index.html donde se implementa la funcion array amigos y la funcion para agregar amigos dentro de la lista, en el index se agrega en imput el elemento para que la funcion pueda identificarlo

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+// Esta función se encarga de agregar un amigo a la lista y actualizar el HTML
+function agregarAmigo() {
+    const nombre = document.getElementById("inputAmigo").value;
+    if (nombre.trim() === "") return;
+    amigos.push(nombre);
+    let elementoHtml = document.getElementById("listaAmigos");
+    elementoHtml.innerHTML = "";
+    amigos.forEach(function(amigo) {
+        elementoHtml.innerHTML += `<li>${amigo}</li>`;
+    });
+    document.getElementById("inputAmigo").value = ""; // Limpia el input
+}
