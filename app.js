dee94c3b48c4d8da2c4f0b8a046601e573e11725
Zefhiros <alejandropizarroleon@gmail.com>
2025-08-01 10:46:03 -0400

    Se agrega la funcion alert para avisar que se ingrese un nombre cuando dejal el campo vacio

--- app.js
+++ app.js
@@ -2,13 +2,16 @@
 let amigos = [];
 // Esta función se encarga de agregar un amigo a la lista y actualizar el HTML
 function agregarAmigo() {
-    const nombre = document.getElementById("inputAmigo").value;
-    if (nombre.trim() === "") return;
-    amigos.push(nombre);
-    let elementoHtml = document.getElementById("listaAmigos");
-    elementoHtml.innerHTML = "";
-    amigos.forEach(function(amigo) {
-        elementoHtml.innerHTML += `<li>${amigo}</li>`;
-    });
-    document.getElementById("inputAmigo").value = ""; // Limpia el input
+  const nombre = document.getElementById("inputAmigo").value;
+  if (nombre.trim() === "") {
+    alert("por favor, inserte un nombre");
+    return;
+  }
+  amigos.push(nombre);
+  let elementoHtml = document.getElementById("listaAmigos");
+  elementoHtml.innerHTML = "";
+  amigos.forEach(function (amigo) {
+    elementoHtml.innerHTML += `<li>${amigo}</li>`;
+  });
+  document.getElementById("inputAmigo").value = ""; // Limpia el input
 }
